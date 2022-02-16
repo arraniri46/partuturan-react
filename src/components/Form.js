d6f5d09b5db548ed61fa104b6c83218f8daf5c23
arraniri46 <garda.arraniri@gmail.com>
2022-02-16 21:55:34 +0700

    still working with form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,5 @@
 import {
   Box,
-  Button,
   Container,
   FormControl,
   FormLabel,
@@ -9,20 +8,31 @@ import {
 import { useEffect, useState } from 'react'
 
 const Form = () => {
-  const [kerabat, setKerabat] = useState([])
+  const [kerabat, setKerabat] = useState('')
+  const [kelamin, setKelamin] = useState('')
+  const [jenjang, setJenjang] = useState('')
+
+  const [data, setData] = useState([])
 
   const getSelect = (e) => e.target.value
 
-  const getValue = (e) => {
-    // if (kerabat.length === 0 || kerabat.every((el) => el !== e.target.value)) {
-    //   setKerabat((kerabat) => [...kerabat, e.target.value])
-    // } else {
-    //   console.log('Data sudah ada!')
-    // }
-    // console.log(kerabat)
-  }
-  console.log(kerabat)
+  // const getValue = (e) => {
+  //   if (kerabat.length === 0 || kerabat.every((el) => el !== e.target.value)) {
+  //     setKerabat((kerabat) => [...kerabat, e.target.value])
+  //   } else {
+  //     console.log('Data sudah ada!')
+  //   }
+  //   console.log(kerabat)
+  // }
 
+  const handleSubmit = (e) => {
+    
+    e.preventDefault()
+
+    setData([kerabat, kelamin, jenjang])
+    
+    console.log(data)
+  }
   return (
     <Container maxW="container.sm" align="center" margin="auto">
       {/* <Box bg="#660708" h={10} mx={24}></Box> */}
@@ -45,37 +55,32 @@ const Form = () => {
           flexDirection="column"
           alignItems="center"
           maxWidth={250}
+          onSubmit={handleSubmit}
         >
+          <form>
           <FormLabel>Kerabat dari siapa?</FormLabel>
-          <Select variant="outline" placeholder="Pilih Kerabat">
+          <Select variant="outline" placeholder="Pilih Kerabat" onChange={(e) => setKerabat(e.target.value)}>
             <option value="ayah">Ayah</option>
             <option value="ibu">Ibu</option>
           </Select>
           <FormLabel>Jenis Kelamin</FormLabel>
-          <Select placeholder="Pilih jenis kelamin">
+          <Select placeholder="Pilih jenis kelamin" onChange={(e) => setKelamin(e.target.value)}>
             <option value="pria">Pria</option>
             <option value="wanita">Wanita</option>
           </Select>
           <FormLabel>Kakak / Adik</FormLabel>
-          <Select placeholder="Pilih jenjang usia">
+          <Select placeholder="Pilih jenjang usia" onChange={(e) => setJenjang(e.target.value)}>
             <option value="kakak">Kakak</option>
             <option value="adik">Adik</option>
           </Select>
+          <button style={{backgroundColor: '#023e8a', color:'white', padding: '10px', marginTop: '36px', borderRadius:'6px'}}>
+            Lihat Hasil
+          </button>
+          </form>
         </FormControl>
 
-        <p>{kerabat}</p>
+        <p>{data}</p>
       </Box>
-      <Button
-        bg="#023e8a"
-        textColor="whiteAlpha.900"
-        variant="solid"
-        size="lg"
-        rounded={0}
-        mt={0}
-        onClick={(e) => getValue(e)}
-      >
-        Lihat Hasil
-      </Button>
     </Container>
   )
 }
