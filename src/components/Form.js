2d344f280b512df6311ff2d13ecb1920e149c2a6
arraniri46 <garda.arraniri@gmail.com>
2022-03-23 22:40:11 +0700

    fix options

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,14 +7,35 @@ import {
 } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
 
+const cekPartuturan = (kerabat) => {
+  if (kerabat.length === 0) return 'Sedang memilih...'
+
+  if (kerabat[0] === 'ayah') {
+    if (kerabat[1] === 'pria') {
+      if (kerabat[2] === 'adik') {
+        return 'Bapak Uda'
+      } else return 'Bapak Tua (Uwak)'
+    } else return 'Namboru (Bou)'
+  } else {
+    if (kerabat[1] === 'wanita') {
+      if (kerabat[2] === 'adik') {
+        return 'Inang Uda'
+      }
+      return 'Inang Tua (Uwak)'
+    }
+    return 'Tulang'
+  }
+}
+
 const Form = () => {
   const [kerabat, setKerabat] = useState('')
   const [kelamin, setKelamin] = useState('')
   const [jenjang, setJenjang] = useState('')
 
   const [data, setData] = useState([])
+  const [hasil, setHasil] = useState('')
 
-  const getSelect = (e) => e.target.value
+  // const getSelect = (e) => e.target.value
 
   // const getValue = (e) => {
   //   if (kerabat.length === 0 || kerabat.every((el) => el !== e.target.value)) {
@@ -26,13 +47,16 @@ const Form = () => {
   // }
 
   const handleSubmit = (e) => {
-    
     e.preventDefault()
 
     setData([kerabat, kelamin, jenjang])
-    
     console.log(data)
   }
+
+  useEffect(() => {
+    setHasil(cekPartuturan(data))
+  }, [data])
+
   return (
     <Container maxW="container.sm" align="center" margin="auto">
       {/* <Box bg="#660708" h={10} mx={24}></Box> */}
@@ -58,28 +82,82 @@ const Form = () => {
           onSubmit={handleSubmit}
         >
           <form>
-          <FormLabel>Kerabat dari siapa?</FormLabel>
-          <Select variant="outline" placeholder="Pilih Kerabat" onChange={(e) => setKerabat(e.target.value)}>
-            <option value="ayah">Ayah</option>
-            <option value="ibu">Ibu</option>
-          </Select>
-          <FormLabel>Jenis Kelamin</FormLabel>
-          <Select placeholder="Pilih jenis kelamin" onChange={(e) => setKelamin(e.target.value)}>
-            <option value="pria">Pria</option>
-            <option value="wanita">Wanita</option>
-          </Select>
-          <FormLabel>Kakak / Adik</FormLabel>
-          <Select placeholder="Pilih jenjang usia" onChange={(e) => setJenjang(e.target.value)}>
-            <option value="kakak">Kakak</option>
-            <option value="adik">Adik</option>
-          </Select>
-          <button style={{backgroundColor: '#023e8a', color:'white', padding: '10px', marginTop: '36px', borderRadius:'6px'}}>
-            Lihat Hasil
-          </button>
+            <FormLabel mt={4}>Kerabat dari siapa?</FormLabel>
+            <Select
+              variant="outline"
+              placeholder="Pilih Kerabat"
+              onChange={(e) => setKerabat(e.target.value)}
+              required
+            >
+              <option value="ayah">Ayah</option>
+              <option value="ibu">Ibu</option>
+            </Select>
+            <FormLabel mt={4}>Jenis Kelamin</FormLabel>
+            <Select
+              placeholder="Pilih jenis kelamin"
+              onChange={(e) => setKelamin(e.target.value)}
+              required
+            >
+              <option value="pria">Pria</option>
+              <option value="wanita">Wanita</option>
+            </Select>
+            <FormLabel mt={4}>Kakak / Adik</FormLabel>
+            <Select
+              placeholder="Pilih jenjang usia"
+              onChange={(e) => setJenjang(e.target.value)}
+              required
+            >
+              <option value="kakak">Abang / Kakak</option>
+              <option value="adik">Adik</option>
+            </Select>
+            <button
+              style={{
+                backgroundColor: '#023e8a',
+                color: 'white',
+                padding: '10px',
+                marginTop: '36px',
+                borderRadius: '6px'
+              }}
+            >
+              Lihat Hasil
+            </button>
           </form>
         </FormControl>
 
-        <p>{data}</p>
+        {!kerabat && (
+          <div
+            style={{
+              backgroundColor: 'red',
+              padding: '20px',
+              marginLeft: '50px',
+              marginRight: '50px',
+              marginBottom: '20px',
+              marginTop: '0px',
+              borderRadius: '8px',
+              color: 'whitesmoke',
+              fontWeight: 'bold'
+            }}
+          >
+            Silahkan isi data!
+          </div>
+        )}
+        {kerabat && (
+          <div
+            style={{
+              backgroundColor: 'green',
+              padding: '20px',
+              marginLeft: '50px',
+              marginRight: '50px',
+              marginBottom: '20px',
+              marginTop: '0px',
+              borderRadius: '8px'
+            }}
+          >
+            <p style={{ color: '#fff' }}>
+              <b>{hasil}</b>
+            </p>
+          </div>
+        )}
       </Box>
     </Container>
   )
